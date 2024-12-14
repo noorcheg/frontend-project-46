a08359ee4ca2e76b5f5d77122632bb71bd600cdf
Ainur Galiakberov <noorcheg@gmail.com>
2024-12-14 19:38:37 +0500

    add args and options

--- gendiff.js
+++ gendiff.js
@@ -5,8 +5,13 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .helpOption('-h, --help', 'output usage information');
+  .name('gendiff') // Название утилиты
+  .usage('[options] <filepath1> <filepath2>') // Строка использования, добавление двух аргументов
+  .description('Compares two configuration files and shows a difference.') // Описание программы
+  .version('0.0.1') // Версия утилиты
+  .argument('<filepath1>', 'path to the first file') // Первый обязательный аргумент
+  .argument('<filepath2>', 'path to the second file') // Второй обязательный аргумент
+  .option('-f, --format [type]', 'output format') // Опция выбора формата
+  .helpOption('-h, --help', 'output usage information'); // Опция справки
 
 program.parse(process.argv);
