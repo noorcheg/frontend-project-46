0acc72edfd807a9a44e528bb7fa59b6d13a82807
Ainur Galiakberov <noorcheg@gmail.com>
2024-12-14 19:10:06 +0500

    add gendiff help

---
+++ gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse(process.argv);
